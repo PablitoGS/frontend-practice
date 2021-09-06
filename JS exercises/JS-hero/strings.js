0a36a71408a849c1c11d1f0899bef712d810d194
Pablo González <pablo.gonzalez@schibsted.com>
2021-09-06 11:01:43 +0200

    ex string

--- JS exercises/JS-hero/strings.js
+++ JS exercises/JS-hero/strings.js
@@ -7,3 +7,13 @@ function indexOfIgnoreCase(s1, s2) {
   let st2 = s2.toLowerCase();
   return st1.indexOf(st2);
 }
+
+//Write a function secondIndexOf,
+//taking two strings and determining the second occurrence of the second string in the first string.
+//If the search string does not occur twice, -1 should be returned.
+
+function secondIndexOf(st1, st2) {
+  let init =  st1.indexOf(st2);
+  let nextPos = st1.indexOf(st2, (init + 1))
+  return nextPos
+}
