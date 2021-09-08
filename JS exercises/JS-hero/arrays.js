a38d24f135f8e29b9c50673685979ad04037eac6
Pablo González <pablo.gonzalez@schibsted.com>
2021-09-08 09:14:49 +0200

    js hero arrays

---
+++ JS exercises/JS-hero/arrays.js
@@ -0,0 +1,8 @@
+// Write a function toArray that takes 2 values and returns these values in an array.
+
+var toArray = (a, b) => {
+	let myArray = [a,b];
+  	return myArray;
+}
+
+console.log(toArray('a', 'b'))
