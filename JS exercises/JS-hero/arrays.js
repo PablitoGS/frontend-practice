6f2e4654719eb5376787a504eabad9fc73aee55e
Pablo González <pablo.gonzalez@schibsted.com>
2021-09-08 11:17:46 +0200

    js hero arrays splice

--- JS exercises/JS-hero/arrays.js
+++ JS exercises/JS-hero/arrays.js
@@ -33,3 +33,25 @@ let getFirst = (array) => {
 const someArray = [24,10,9,2,3,5];
 
 console.log(getFirst(someArray))
+
+
+//Write a function setFirstElement that takes an array and an arbitrary variable.
+//The variable should be inserted as the first element in the array. The array should be returned.
+
+const numbers = [4,2,3];
+
+let  setFirstElement = (array, number) => {
+  array.splice(0,1, number);
+  return arrray
+  };
+
+console.log(setFirstElement(numbers, 1.1))
+
+//..//
+
+function setFirstEl(array, firstElement) {
+  array[0] = firstElement;
+  return array;
+}
+
+console.log(setFirstEl(numbers, 1))
