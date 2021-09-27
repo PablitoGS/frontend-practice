c094d0ab01c39c24c43c00b03442a9ad78307d4a
Pablo González <pablo.gonzalez@schibsted.com>
2021-09-27 08:51:59 +0200

    promesa con fetch

---
+++ JS exercises/promises/lyricsRepository.js
@@ -0,0 +1,23 @@
+
+class lyrics {
+    execute({author, song}) {
+      const url = `https://api.lyrics.ovh/v1/${author}/${song}`;
+      return fetch(url)
+        .then((response) => response.json())
+        .catch((err) => console.error("The song was not found"));
+    }
+  }
+  
+  let lyricsRepository = new lyrics()
+  
+  lyricsRepository.execute(
+    {   
+        author: "kiss", 
+        song: "strutter"}
+    )
+  
+  .then((data) => {
+    document.getElementById("app").innerHTML = `${data.lyrics}`;
+  })
+  .catch((err) => console.error("La canción no existe"));
+  
