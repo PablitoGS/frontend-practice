be3a8a8d0790f30a980732da8af30b39c9735d67
Pablo González <pablo.gonzalez@schibsted.com>
2021-10-06 17:50:52 +0200

    refactor

--- JS exercises/promises/promise-all.js
+++ JS exercises/promises/promise-all.js
@@ -1,61 +1,30 @@
+const axios = require("axios");
 
-promise()
+const urlUsers = "https://jsonplaceholder.typicode.com/users";
+const urlAlbums = "https://jsonplaceholder.typicode.com/albums";
+
+promise().then((res) => console.log(res));
 
 function getNames() {
-  const url = "https://jsonplaceholder.typicode.com/users";
   return axios
-  .get(url, { responseType: "json" })
-  .then(function (response) {
-    return response.data;
-  })
-  .then(function (data) {
-    let arr = []
-    data.map((data) => arr.push(data.name))
-    return arr
-  })
+    .get(urlUsers, { responseType: "json" })
+    .then(function (response) {
+      return response.data.map((value) => value.name);
+    });
 }
 
 function getAlbums() {
-  const url = "https://jsonplaceholder.typicode.com/albums";
   return axios
-  .get(url, { responseType: "json" })
-  .then(function (response) {
-    return response.data;
-  })
-  .then(function (data) {
-    let arr = []
-    data.map((data) => arr.push(data.title ))
-    return arr
-
-  })
+    .get(urlAlbums, { responseType: "json" })
+    .then(function (response) {
+      return response.data.map((value) => value.title);
+    });
 }
-
-
-// async function promise() {
-//   return await Promise.all([getNames(), getAlbums()]).then(
-//
-//     function([names, albums]) {
-//       const arr = []
-//       names.map(
-//         function(name, i) {
-//           let album = albums[i];
-//           arr.push({name,album})
-//           return arr
-//         }
-//       )
-//       console.log(arr)
-//      return arr
-//     }
-//   )
-// }
 
 async function promise() {
   const [names, albums] = await Promise.all([getNames(), getAlbums()]);
-  const arr = [];
-  names.map(function (name, i) {
+  return names.map(function (name, i) {
     let album = albums[i];
-    arr.push({ name, album });
-    return arr;
+    return { name, album };
   });
-  return arr;
 }
