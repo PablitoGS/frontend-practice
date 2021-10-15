f27e022b6fe2179f2d7f1c2b42133d336393bee5
Pablo González <pablo.gonzalez@schibsted.com>
2021-10-15 12:51:15 +0200

    hooks

--- react-frontend-masters/adopt-me/src/App.js
+++ react-frontend-masters/adopt-me/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Gardfield" animal="cat" breed="Orange Persian" />
-      <Pet name="Perales" animal="bird" breed="Canario" />
+      <SearchParams />;
     </div>
   );
 };
