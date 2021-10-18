fc96a4b206abfb148415932605aab2d70f93d5bf
Pablo González <pablo.gonzalez@schibsted.com>
2021-10-18 16:57:34 +0200

    remove function

--- react-frontend-masters/adopt-me/src/SearchParams.js
+++ react-frontend-masters/adopt-me/src/SearchParams.js
@@ -5,8 +5,8 @@ import useBreedList from "./useBreedList";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
+  const [location, setLocation] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
   const [breeds] = useBreedList(animal);
@@ -23,10 +23,6 @@ const SearchParams = () => {
     setPets(json.pets);
   }
 
-  function updateLocation(e) {
-    setLocation(e.target.value);
-  }
-
   return (
     <div className="search-params">
       <form>
@@ -34,7 +30,7 @@ const SearchParams = () => {
           Location
           <input
             id="location"
-            onChange={updateLocation}
+            onChange={(e) => setLocation(e.target.value)}
             value={location}
             placeholder="Location"
           />
