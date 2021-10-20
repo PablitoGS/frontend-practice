50200f35ee9063ac6959ec2037f084ada4861d62
Pablo González <pablo.gonzalez@schibsted.com>
2021-10-20 17:19:53 +0200

    fix

--- react/search-gifs/search-gifs/src/App.js
+++ react/search-gifs/search-gifs/src/App.js
@@ -1,13 +1,40 @@
-//import Button from './Button';
+import Button from "./Button";
 //import InputText from './InputText';
-import GifList from "./GifList";
+import useGifList from "./GifList";
+import { useState } from "react";
+
 import "./App.css";
 
 function App() {
+  const [search, updateValue] = useState("");
+  const [input, updateInput] = useState("");
+  const [gifs] = useGifList({ search });
+
   return (
     <div className="App">
       <section>
-        <GifList />
+        <div className="section">
+          <div className="inputText">
+            <label>Busca tu GIF</label>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => updateInput(e.target.value)}
+            ></input>
+          </div>
+          <Button
+            text="Me siento con suerte"
+            onClick={(e) => {
+              e.preventDefault();
+              updateValue(input);
+            }}
+          />
+          <div className="gifList">
+            {gifs.map((gif, index) => (
+              <img key={index} alt="" src={gif} />
+            ))}
+          </div>
+        </div>
       </section>
     </div>
   );
--- react/search-gifs/search-gifs/src/GifList.js
+++ react/search-gifs/search-gifs/src/GifList.js
@@ -1,48 +1,25 @@
-import Button from "./Button";
 import { useState, useEffect } from "react";
 
-const GifList = () => {
+const useGifList = ({ search = "cats" } = {}) => {
   const [gifs, updateGifs] = useState([]);
-  const [inputvalue, updateValue] = useState("");
 
-  useEffect(function ({ search } = {}) {
-    requestGifs();
-  }, []);
+  useEffect(
+    function () {
+      requestGifs();
 
-  async function requestGifs({ search } = {}) {
-    const res = await fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=Qcfkd8glFPZuGsVzzDXNVvKCKylzxtWf&limit=10&q=${search}`
-    );
-    const json = await res.json();
-    const data = await json.data;
-    const imgs = data.map((gif) => gif.images.downsized.url);
-    updateGifs(imgs);
-  }
-
-  return (
-    <div className="section">
-      <div className="inputText">
-        <label>Busca tu GIF</label>
-        <input
-          type="text"
-          value={inputvalue}
-          onChange={(e) => updateValue(e.target.value)}
-        ></input>
-      </div>
-      <Button
-        text="Me siento con suerte"
-        onClick={(e) => {
-          e.preventDefault();
-          requestGifs({ search: inputvalue });
-        }}
-      />
-      <div className="gifList">
-        {gifs.map((gif, index) => (
-          <img key={index} alt="" src={gif} />
-        ))}
-      </div>
-    </div>
+      async function requestGifs() {
+        const res = await fetch(
+          `https://api.giphy.com/v1/gifs/search?api_key=Qcfkd8glFPZuGsVzzDXNVvKCKylzxtWf&limit=10&q=${search}`
+        );
+        const json = await res.json();
+        const data = await json.data;
+        const imgs = await data.map((gif) => gif.images.downsized.url);
+        updateGifs(imgs);
+      }
+    },
+    [search]
   );
+  return [gifs];
 };
 
-export default GifList;
+export default useGifList;
