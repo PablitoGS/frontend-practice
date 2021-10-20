2a77a35c3157349b7bbb445c10672704c0b5f428
Pablo González <pablo.gonzalez@schibsted.com>
2021-10-20 11:52:31 +0200

    refactor

--- react-frontend-masters/adopt-me/src/Pet.js
+++ react-frontend-masters/adopt-me/src/Pet.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Pet = ({ name, animal, breed }) => {
+const Pet = ({ name, animal, breed, location }) => {
   return (
     <div>
       <h2>{name}</h2>
       <h3>{animal}</h3>
       <h3>{breed}</h3>
+      <span>{location}</span>
     </div>
   );
 };
