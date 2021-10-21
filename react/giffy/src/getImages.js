cc40347f1e35ddbc7264157f3eceba7de89f1f09
Pablo González <pablo.gonzalez@schibsted.com>
2021-10-21 14:02:35 +0200

    update con async

--- react/giffy/src/getImages.js
+++ react/giffy/src/getImages.js
@@ -1,13 +1,10 @@
-export default function getImages(url) {
-  return fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      data.splice(5)
-      const images = data.map(value => {
-        const {thumbnailUrl, title} = value
-        return {thumbnailUrl, title}
-      })
-
-      return images
+export default async function getImages(url) {
+  const res = await (await fetch(url)).json()
+  res.splice(5)
+  const images = res.map(value => {
+    const {thumbnailUrl, title} = value
+     return {thumbnailUrl, title}
     })
+    
+  return images
 }
