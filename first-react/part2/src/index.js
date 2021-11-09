db2c12998bd0a8b2e53ba81e38a90408f59e4043
Pablo González <pablo.gonzalez@schibsted.com>
2021-11-09 17:26:30 +0100

    state

--- first-react/part2/src/index.js
+++ first-react/part2/src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {useState} from 'react'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { useState } from "react";
+
+const Counter = ({ number }) => <h1>El valor es: {number}</h1>;
 
 const App = (props) => {
   //const counter = useState(0) //valor inicial del estado
@@ -10,22 +12,70 @@ const App = (props) => {
   //const valueCounter = counter[0] //Primera posision es valor del estado
   //const updateCounter = counter[1] //Segunda posicion Metodo que al ejecutarlo podemos actualizar el estado
 
-  const [valueCounter, updateCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
+  const [newCounter, setNewCounter] = useState({
+    left: 0,
+    right: 0,
+    clicks: 0,
+    message: "Mensaje de estado",
+  });
+
+  const eClickLeft = () => {
+    setNewCounter({
+      ...newCounter,
+      left: newCounter.left + 1,
+      clicks: newCounter.clicks + 1,
+    });
+  };
+
+  const eClickRight = () => {
+    setNewCounter({
+      ...newCounter,
+      right: newCounter.right + 1,
+      clicks: newCounter.clicks + 1,
+    });
+  };
+
+  const handleClick = (param) => {
+    if (param === "down") {
+      return () => setCounter((prevNumber) => prevNumber - 1);
+    }
+    if (param === "up") {
+      return () => setCounter((prevNumber) => prevNumber + 1);
+    }
+  };
+
+  function handleClickReset() {
+    setCounter(0);
+  }
+
+  const isEven = counter % 2 === 0;
 
   return (
     <>
-    <h1>{valueCounter}</h1>
-    <button onClick={() =>
-      updateCounter(valueCounter+1)}
-      >Incrementar</button>
+      <Counter number={counter} />
+      <p>El número es {isEven ? "par" : "impar"}</p>
+      <div>
+        <button onClick={handleClick("up")}>Incrementar</button>
+        <button onClick={handleClick("down")}>Decrementar</button>
+        <button onClick={handleClickReset}>Reset</button>
+      </div>
+      <hr></hr>
+      <div>
+        {newCounter.left}
+        <button onClick={eClickLeft}>left</button>
+        <button onClick={eClickRight}>right</button>
+        {newCounter.right}
+        <p>Click totales: {newCounter.clicks}</p>
+        <p>{newCounter.message}</p>
+      </div>
     </>
-  )
-}
-
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root') 
+  document.getElementById("root")
 );
